837cef61ccaa76de4a30e281302ec113153307dc
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2014-12-10 23:10:58 +0100

    Moved map script. Enabled uglification.

--- src/js/content.js
+++ src/js/content.js
@@ -19,74 +19,76 @@ yepnope.injectJs('static/vendors.js', function () {
     $(function () {
         FastClick.attach(document.body);
     });
+
+    yepnope.injectJs('https://maps.googleapis.com/maps/api/js?v=3.exp&callback=initializeMap', function () {
+        window.initializeMap = function () {
+            var map;
+
+            function initialize() {
+                var mapOptions = {
+                    zoom: 16,
+                    center: new google.maps.LatLng(52.219766, 21.021036),
+                    disableDefaultUI: true,
+                    draggable: false,
+                    zoomControl: false,
+                    scrollwheel: false,
+                    disableDoubleClickZoom: true
+                };
+
+                google.maps.Map.prototype.setCenterWithOffset = function (latlng, offsetX, offsetY) {
+                    var map = this;
+                    var ov = new google.maps.OverlayView();
+                    ov.onAdd = function () {
+                        var proj = this.getProjection();
+                        var aPoint = proj.fromLatLngToContainerPixel(latlng);
+                        aPoint.x = aPoint.x + offsetX;
+                        aPoint.y = aPoint.y + offsetY;
+                        map.setCenter(proj.fromContainerPixelToLatLng(aPoint));
+                    };
+                    ov.draw = function () {
+                    };
+                    ov.setMap(this);
+                };
+
+                map = new google.maps.Map(document.getElementById('map'), mapOptions);
+                map.setCenterWithOffset(new google.maps.LatLng(52.219766, 21.021036), -$(window).width() / 4, 0);
+
+                setTimeout(function () {
+                    new google.maps.Marker({
+                        position: new google.maps.LatLng(52.219766, 21.021036),
+                        map: map,
+                        draggable: false,
+                        animation: google.maps.Animation.DROP
+                    });
+                }, 400);
+
+                $('#map').data('initialized', true);
+            }
+
+            function showMapIfNeeded() {
+                if (currentScreenSize() !== 'small') {
+                    var map = $('#map');
+                    map.show();
+                    if (!map.data('initialized')) {
+                        google.maps.event.addDomListener(window, 'load', initialize);
+                        initialize();
+                    }
+                } else {
+                    $('#map').hide();
+                }
+            }
+
+            function currentScreenSize() {
+                return $('#media-query-breakpoints').find('div:visible').first().data('size');
+            }
+
+            $(window).resize(function () {
+                showMapIfNeeded();
+            });
+            showMapIfNeeded();
+        }
+    });
 });
 
 yepnope.injectJs('static/google-analytics.js');
 
-yepnope.injectJs('https://maps.googleapis.com/maps/api/js?v=3.exp&callback=initializeMap', function () {
-    window.initializeMap = function () {
-        var map;
-        function initialize() {
-            var mapOptions = {
-                zoom: 16,
-                center: new google.maps.LatLng(52.219766, 21.021036),
-                disableDefaultUI: true,
-                draggable: false,
-                zoomControl: false,
-                scrollwheel: false,
-                disableDoubleClickZoom: true
-            };
-
-            google.maps.Map.prototype.setCenterWithOffset = function (latlng, offsetX, offsetY) {
-                var map = this;
-                var ov = new google.maps.OverlayView();
-                ov.onAdd = function () {
-                    var proj = this.getProjection();
-                    var aPoint = proj.fromLatLngToContainerPixel(latlng);
-                    aPoint.x = aPoint.x + offsetX;
-                    aPoint.y = aPoint.y + offsetY;
-                    map.setCenter(proj.fromContainerPixelToLatLng(aPoint));
-                };
-                ov.draw = function () {
-                };
-                ov.setMap(this);
-            };
-
-            map = new google.maps.Map(document.getElementById('map'), mapOptions);
-            map.setCenterWithOffset(new google.maps.LatLng(52.219766, 21.021036), -$(window).width() / 4, 0);
-
-            setTimeout(function () {
-                new google.maps.Marker({
-                    position: new google.maps.LatLng(52.219766, 21.021036),
-                    map: map,
-                    draggable: false,
-                    animation: google.maps.Animation.DROP
-                });
-            }, 400);
-
-            $('#map').data('initialized', true);
-        }
-
-        function showMapIfNeeded() {
-            if (currentScreenSize() !== 'small') {
-                var map = $('#map');
-                map.show();
-                if (!map.data('initialized')) {
-                    google.maps.event.addDomListener(window, 'load', initialize);
-                    initialize();
-                }
-            } else {
-                $('#map').hide();
-            }
-        }
-
-        function currentScreenSize() {
-            return $('#media-query-breakpoints').find('div:visible').first().data('size');
-        }
-
-        $(window).resize(function () {
-            showMapIfNeeded();
-        });
-        showMapIfNeeded();
-    }
-});
